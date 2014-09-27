b6d1034445bc83a9d4477318627391deb2c3d133
Anwesha Deb <anwesha@yahoo-inc.com>
2014-09-27 16:35:13 -0700

    use dust

--- app.js
+++ app.js
@@ -1,39 +1,83 @@
 var express = require('express'),
     app     = express(),
-    request = require('request');
+    request = require('request'),
+    async   = require("async"),
+    dust    = require('dustjs-linkedin'),
+    cons    = require('consolidate');
 
+    app.engine('dust', cons.dust);;
+
+function createUrl( state , city) {
+    var weatherUrl = 'http://api.wunderground.com/api/d45432e79a514535/geolookup/conditions/lang:EN/q/'+state+'/'+city+'.json';
+    return weatherUrl;
+}
 
 var server = app.listen(3000, function() {
 
-    var apiUrl = 'http://api.wunderground.com/api/d45432e79a514535/geolookup/conditions/lang:EN/q/CA/San_Francisco.json';
-
-     // set templating engine and jade
-     app.set('views', './views');
-     app.set('view engine', 'jade');
-     app.engine('jade', require('jade').__express);
+     // set templating engine
+    app.set('views', __dirname + '/views');
+    app.set('view engine','dust');
+    app.engine('dust', cons.dust);
 
     // register default route
     app.get('/', function (req, res) {
 
-        // make hardcoded basic api call to lookup sfo's weather
-        request(apiUrl, function (err, response, body) {
+        var tasks,
+            weatherPoints = {
+            "CA" : "Campbell",
+            "NE" : "Omaha",
+            "TX" : "Austin",
+            "MD" : "Timonium"
+        };
 
-          var json = {},
-              data = {};
+        var getWeatherData = function (state, doneCallback) {
+            var apiUrl = createUrl(state, weatherPoints[state]);
 
-        if (!err && response.statusCode == 200) {
+            request(apiUrl, function (err, response, body) {
+                var json = {};
+                var obj = {};
+                if (!err && response.statusCode == 200) {
 
-            json = JSON.parse(body);
+                      json = JSON.parse(body);
 
-            // TODO : do defensive checks
-            data = {
-                'city'  : json.location.city,
-                'state' : json.location.state,
-                'temp'  : json.current_observation.temperature_string
-            };
+                      // TODO : do defensive checks
+                      obj = {
+                        'city'  : json.location.city,
+                        'state' : json.location.state,
+                        'temp'  : json.current_observation.temperature_string
+                      };
 
+                      doneCallback(null, obj);
+                }
+            });
+        };
+
+        tasks = {
+            "CA" : function (callback) {
+                getWeatherData("CA", callback);
+            },
+            "NE" : function (callback) {
+                getWeatherData("NE", callback);
+            },
+            "TX" : function (callback) {
+                getWeatherData("TX", callback);
+            },
+            "MD" : function (callback) {
+                getWeatherData("MD", callback);
+            }
+        }
+
+        async.series(tasks, function (err, response) {
+
+            var data = {};
+                weatherData = [];
+            weatherData.push(response.CA);
+            weatherData.push(response.NE);
+            weatherData.push(response.TX);
+            weatherData.push(response.MD);
+
+            data.weatherData = weatherData;
             res.render('weather', data);
-          }
         });
-    });
+   });
 });
